cf54b21fd788906968689fb533152159b16e5add
shaoguyi <114022474+shaoguyi@users.noreply.github.com>
2024-02-28 01:23:55 -0500

    Delete Lab4 directory

--- Lab4/yatzy_game.js
+++
@@ -1,9 +0,0 @@
-function toggleLock(diceNumber) {
-    diceLocked[diceNumber - 1] = !diceLocked[diceNumber - 1];
-    const button = document.getElementById(`dice${diceNumber}`).querySelector("button");
-    if (diceLocked[diceNumber - 1]) {
-      button.textContent = "Unlock";
-    } else {
-      button.textContent = "Lock";
-    }
-  }
